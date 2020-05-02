6b99892c4726f872750feca95d761f141d4472e2
Pradeep Joam <pradeepjoam@gmail.com>
2020-05-02 06:38:23 +0530

    Update Employee.java

--- src/main/java/com/pradeepjoam/jpaquery/entities/Employee.java
+++ src/main/java/com/pradeepjoam/jpaquery/entities/Employee.java
@@ -10,7 +10,7 @@ import javax.persistence.Table;
 import org.springframework.stereotype.Component;
 
 @Entity
-@Table(name="TechEmp")
+@Table(name="Employee")
 @Component
 public class Employee {
 	
